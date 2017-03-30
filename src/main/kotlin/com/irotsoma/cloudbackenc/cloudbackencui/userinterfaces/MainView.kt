86a9f6e8808c89ccabfeb290fa1540413e9fa4ae
irotsoma <irotsomadev@gmail.com>
2017-03-30 13:59:33 -0700

    Work on UI login flow.

--- src/main/kotlin/com/irotsoma/cloudbackenc/cloudbackencui/userinterfaces/MainView.kt
+++ src/main/kotlin/com/irotsoma/cloudbackenc/cloudbackencui/userinterfaces/MainView.kt
@@ -19,12 +19,24 @@
 
 package com.irotsoma.cloudbackenc.cloudbackencui.userinterfaces
 
+import com.irotsoma.cloudbackenc.cloudbackencui.CentralControllerRestInterface
+import com.irotsoma.cloudbackenc.cloudbackencui.UserAccount
+import com.irotsoma.cloudbackenc.cloudbackencui.UserAccountManager
+import com.irotsoma.cloudbackenc.cloudbackencui.trustSelfSignedSSL
+import com.irotsoma.cloudbackenc.common.AuthenticationToken
 import javafx.scene.control.MenuItem
 import javafx.scene.layout.VBox
 import javafx.stage.Modality
 import javafx.stage.StageStyle
+import mu.KLogging
+import org.apache.tomcat.util.codec.binary.Base64
 import org.springframework.context.annotation.Lazy
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
 import tornadofx.View
 import tornadofx.get
 
@@ -39,9 +51,12 @@ import tornadofx.get
 @Lazy
 @Component
 class MainView : View() {
+    /** kotlin-logging implementation*/
+    companion object: KLogging()
     override val root: VBox by fxml()
     final val menuCloudServicesSetup : MenuItem by fxid("menuCloudServicesSetup")
     final val menuUsersCreateUser : MenuItem by fxid("menuUsersCreateUser")
+    final val menuUsersLogin: MenuItem by fxid("menuUsersLogin")
     init{
         title = messages["cloudbackencui.title.application"]
         menuCloudServicesSetup.setOnAction{
@@ -49,6 +64,33 @@ class MainView : View() {
         }
         menuUsersCreateUser.setOnAction {
             CreateUserFragment().openModal(StageStyle.UTILITY, Modality.APPLICATION_MODAL, false)
+        }
+        menuUsersLogin.setOnAction {
+            val userInfoPopup = UserInfoFragment(messages["cloudbackencui.title.product"]).apply{openModal(StageStyle.UTILITY,Modality.APPLICATION_MODAL, false, this.currentWindow, true)}
+            if (userInfoPopup.username != null && userInfoPopup.password != null) {
+                val user = userInfoPopup.username!!
+                val plainUserCredentials = "$user:${userInfoPopup.password}".toByteArray()
+                val base64UserCredentials = String(Base64.encodeBase64(plainUserCredentials))
+                val tokenRequestHeaders = HttpHeaders()
+                tokenRequestHeaders.add(HttpHeaders.AUTHORIZATION, "Basic $base64UserCredentials")
+                val httpTokenEntity = HttpEntity<Any>(tokenRequestHeaders)
+                runAsync {
+                    val restInterface = CentralControllerRestInterface()
+                    //for testing use a hostname verifier that doesn't do any verification
+                    if ((restInterface.centralControllerSettings!!.useSSL) && (restInterface.centralControllerSettings!!.disableCertificateValidation)) {
+                        trustSelfSignedSSL()
+                        logger.warn { "SSL is enabled, but certificate validation is disabled.  This should only be used in test environments!" }
+                    }
+                    val tokenResponse = RestTemplate().exchange("${restInterface.centralControllerProtocol}://${restInterface.centralControllerSettings!!.host}:${restInterface.centralControllerSettings!!.port}/auth", HttpMethod.GET, httpTokenEntity, AuthenticationToken::class.java)
+                    if (tokenResponse.statusCode == HttpStatus.OK) {
+                        //update or insert user in database
+                        val userAccountRepository = UserAccountManager().userAccountRepository
+                        val userAccount = userAccountRepository.findByUsername(user) ?: UserAccount(user, tokenResponse.body.token)
+                        userAccountRepository.save(userAccount)
+                        //TODO: add success message popup
+                    }
+                }
+            }
         }
     }
 
